cf25cc37626cb7e602e055f41d57dd5c697a5964
Playmaker0210 <tenbin2003@gmail.com>
2023-05-15 01:53:20 +0700

    up

--- src/main/java/com/example/app1/MyJavaFXApp.java
+++ src/main/java/com/example/app1/MyJavaFXApp.java
@@ -14,19 +14,15 @@ import java.util.Objects;
 public class MyJavaFXApp extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
-        // Tạo Scene với BorderPane của UI làm nội dung
         FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("UI.fxml")));
         Parent root = loader.load();
         Scene scene = new Scene(root);
 
-        // Đặt tiêu đề cho cửa sổ ứng dụng
         primaryStage.setTitle("JavaFX Book Reader");
         primaryStage.setResizable(false);
 
-        // Đặt Scene làm nội dung cho cửa sổ ứng dụng
         primaryStage.setScene(scene);
 
-        // Hiển thị cửa sổ ứng dụng
         primaryStage.show();
     }
 
